1eacedbc21695a060758dae4630290832a3e9160
Yuri Tsukahara <yuritsukahara@outlook.com>
2022-01-01 15:38:27 -0300

    Init

---
+++ color_box/app.js
@@ -0,0 +1,32 @@
+const colors = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'purple',
+    'indigo',
+    'violet',
+];
+
+const changeColor = function(box){
+   h1.style.color = this.style.backgroundColor;
+
+
+}
+
+const h1 =  document.querySelector('h1')
+
+h1.addEventListener('mouseover', function(){
+    console.log(this.innerText);
+})
+
+const container = document.querySelector('#boxes');
+
+for (let color of colors){
+    const box = document.createElement('div');
+    box.style.backgroundColor = color;
+    box.classList.add('box');
+    container.appendChild(box);
+    box.addEventListener('mouseover', changeColor);
+}
