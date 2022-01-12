61dc12bf7c8604e4be73ce25caec11ba26751778
Yuri Tsukahara <yuritsukahara@outlook.com>
2022-01-11 23:35:19 -0300

    movie fight study

---
+++ part_2/movie_fight/index.js
@@ -0,0 +1,52 @@
+const fetchData = async (searchTerm) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "9f2ac430",
+      s: searchTerm,
+    },
+  });
+
+  if (response.data.Error) {
+    return [];
+  }
+
+  return response.data.Search;
+};
+
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+    <label><b> Search For a Movie</b></label>
+    <input class="input"/>
+    <div class="dropdown is-active">
+        <div class="dropdown-menu">
+            <div class="results">
+            </div>
+        </div>
+    </div>
+`;
+
+const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
+
+const onInput = async (event) => {
+  const movies = await fetchData(event.target.value);
+
+  resultsWrapper.innerHTML='';
+
+  dropdown.classList.add("is-active");
+  for (let movie of movies) {
+    const option = document.createElement("a");
+
+    const imgSrc = movie.Poster === "N/A" ? '': movie.Poster;
+
+    option.classList.add("dropdown-item");
+    option.innerHTML = `
+        <img src="${imgSrc}" />
+        ${movie.Title}
+    `;
+    resultsWrapper.appendChild(option);
+  };
+};
+
+input.addEventListener("input", debounce(onInput, 500));
