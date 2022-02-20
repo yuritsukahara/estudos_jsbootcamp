2e48d4fb61a8357b4e196efce00d4477321afe95
Yuri Tsukahara <yuritsukahara@outlook.com>
2022-02-19 23:54:52 -0300

    products implementation

--- part_3/ecommerce/routes/admin/auth.js
+++ part_3/ecommerce/routes/admin/auth.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator');
-
+const {handleErrors} = require('./middlewares');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -21,12 +20,8 @@ router.get('/signup', (req, res) => {
 router.post(
 	'/signup',
 	[requireEmail, requirePassword, requirePasswordConfirmation],
+	handleErrors(signupTemplate),
 	async (req, res) => {
-		const errors = validationResult(req);
-		console.log('errors :>> ', errors);
-		if (!errors.isEmpty()) {
-			return res.send(signupTemplate({ req, errors }));
-		}
 
 		const { email, password, passwordConfirmation } = req.body;
 		const user = await usersRepo.create({ email, password });
@@ -49,12 +44,8 @@ router.get('/signin', (req, res) => {
 router.post(
 	'/signin',
 	[requireEmailExists, requireValidPasswordForUser],
+	handleErrors(signinTemplate),
 	async (req, res) => {
-		const errors = validationResult(req);
-
-		if (!errors.isEmpty()) {
-			return res.send(signinTemplate({ errors }));
-		}
 
 		const { email } = req.body;
 
